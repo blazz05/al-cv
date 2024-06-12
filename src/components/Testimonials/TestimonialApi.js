c65dc6683fd1d474aa019e3bd662c7d2c1000aeb
Aldi Budiansah <129393970+blazz05@users.noreply.github.com>
2024-06-12 11:14:05 +0700

    cv

---
+++ src/components/Testimonials/TestimonialApi.js
@@ -0,0 +1,35 @@
+const TestimonialApi = [
+    {
+      id: 1,
+      image: "./image/sertifikat.jpg",
+      // design: "NCD - DESIGN",
+      // name: "Junior Web Developer",
+      // offcer: "Marketing Officer",
+      // post: "CEO - Marketing",
+      // date: "Thoda Department - Mar 4, 2018 - Aug 30, 2021",
+      desc: "Here I took the Junior Web Developer certificate at UTAMA INFORMATICS VOCATIONAL SCHOOL, DEPOK in 2023",
+    },
+    {
+      id: 2,
+      image: "./image/minecraft.png",
+      // design: "NCD - DESIGN",
+      // name: "Minecraft",
+      // offcer: "Marketing Officer",
+      // post: "CEO - Marketing",
+      // date: "Thoda Department - Mar 4, 2018 - Aug 30, 2021",
+      desc: "I got this Minecraft certificate by playing the game on the Minecraft website, where we learn programming language algorithms",
+    },
+    {
+      id: 3,
+      image: "./image/dcoding-live.png",
+      // design: "NCD - DESIGN",
+      // name: "Developer Coaching",
+      // offcer: "Marketing Officer",
+      // post: "CEO - Marketing",
+      // date: "Thoda Department - Mar 4, 2018 - Aug 30, 2021",
+      desc: "I got this dcoding certificate by attending an online seminar where we explained about Dcoding Developer Coaching",
+    },
+  ]
+  
+  export default TestimonialApi
+  
